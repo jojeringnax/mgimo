796084564ece952c474c14d85b3c68c0ee86ffdc
jojeringnax <hollow718@gmail.com>
2018-12-25 19:30:14 +0300

    Clknfsaekjgnlke

--- database/migrations/2018_12_24_132855_add_column_priority_to_congratulations.php
+++ database/migrations/2018_12_24_132855_add_column_priority_to_congratulations.php
@@ -26,7 +26,7 @@ class AddColumnPriorityToCongratulations extends Migration
     public function down()
     {
         Schema::table('congratulations', function (Blueprint $table) {
-            $table->dropColumn('tinyInteger');
+            $table->dropColumn('priority');
         });
     }
 }
