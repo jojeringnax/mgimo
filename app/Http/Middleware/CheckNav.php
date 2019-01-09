959cc7047976463ce1bf8a995945e9d1df164c22
jojeringnax <hollow718@gmail.com>
2019-01-09 23:20:15 +0300

    fix active links

--- app/Http/Middleware/CheckNav.php
+++ app/Http/Middleware/CheckNav.php
@@ -16,8 +16,7 @@ class CheckNav
     public function handle($request, Closure $next)
     {
         $active = explode('/',$request->url());
-        $public = count($active) >= 3 ? $active[3] === 'public' : false;
-        if (($public && count($active) <= 4) || (!$public && count($active) <= 3)) {
+        if (($active[3] === 'public' && count($active) <= 4) || (!$active[2] === 'public' && count($active) <= 3)) {
             $result = 'main';
         } else {
             $result = $public ? $active[4] : $active[3];
