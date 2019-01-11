ec70588241f17e99dc0df89c330a6ffd867fbfc8
jojeringnax <hollow718@gmail.com>
2019-01-11 13:41:20 +0300

    LOcations Events

--- app/helpers.php
+++ app/helpers.php
@@ -1,15 +1,16 @@
 <?php
 if (!function_exists('cut_html')) {
-    function cut_html(string $html, $length=320) {
-        $forIter = strlen($html)-$length;
+    function cut_html(string $html, $offset=250) {
+        $forIter = strlen($html)-$offset;
         for($i=0;$i<$forIter;$i++) {
-            $g = substr($html, $length-1, 1);
-            $res[] = $g;
-            if (!in_array($g, array('!', '.', '?'))) {
-               $length++;
+            $g = substr($html, $offset-1, 15);
+            $result = preg_match('/<\/[a-z]+>/', $g, $matches, PREG_OFFSET_CAPTURE);
+            if ($result !== 0) {
+                $offset = $offset + $matches[0][1] + strlen($matches[0][0]);
+                return html_entity_decode(substr($html, 0, $offset+1));
             } else {
-                return html_entity_decode(substr($html, 0, $length));
-            };
+                $offset += 15;
+            }
         }
         return html_entity_decode($html);
     }
